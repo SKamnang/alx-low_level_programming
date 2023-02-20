da57f4f4ae30ca69cccb5b01e803c27b48e557b0
Stephane KAMNANG <stephanekamnang@gmail.com>
2023-02-20 05:43:31 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  * main - prints a line to the standard error
  * Return: 1 (success)
  */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return: (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
