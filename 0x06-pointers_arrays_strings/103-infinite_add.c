76932cf97555b459c72f784d20069724d8f7fb61
Stephane KAMNANG <stephanekamnang@gmail.com>
2023-03-02 16:47:10 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -37,6 +37,7 @@ void rev_string(char *n)
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
@@ -76,4 +77,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
-
+}
