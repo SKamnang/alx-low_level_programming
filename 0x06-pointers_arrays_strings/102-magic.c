6cc6af1ee85d7073518ae448dc9cbcd4c05e9b4b
Stephane KAMNANG <stephanekamnang@gmail.com>
2023-03-02 17:52:59 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
 
-/**
- * main - program that prints a[2] = 98
- * Return: always 0 (Success)
- */
 int main(void)
 {
 	int n;
@@ -21,8 +17,8 @@ int main(void)
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
-*(p + 5) = 98;
+	*(p + 5) = 98;
 /* ...so that this prints 98\n */
-printf("a[2] - %d\n", a[2]);
-return (0);
+	printf("a[2] - %d\n", a[2]);
+	return (0);
 }
